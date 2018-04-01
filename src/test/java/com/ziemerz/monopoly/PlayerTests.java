718d386d8fc65162c56b3790b21dfcb9f12aa492
Mikkel Ziemer <mzhjensen@gmail.com>
2018-04-01 20:26:39 +0200

    Cleaned up imports

--- src/test/java/com/ziemerz/monopoly/PlayerTests.java
+++ src/test/java/com/ziemerz/monopoly/PlayerTests.java
@@ -2,17 +2,14 @@ package com.ziemerz.monopoly;
 
 import com.ziemerz.monopoly.model.*;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import static org.mockito.Mockito.*;
 
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,6 +20,7 @@ import static org.junit.Assert.*;
 @ActiveProfiles("test")
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PlayerTests {
 
     @Inject
